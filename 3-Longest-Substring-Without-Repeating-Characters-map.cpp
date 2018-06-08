6b0c73dfb2f37bd354d7f8ff5459c0b466dd754e
John Hany <johnhany@163.com>
2018-06-08 13:59:12 +0800

    Create 3-Longest-Substring-Without-Repeating-Characters-map.cpp

---
+++ 3-Longest-Substring-Without-Repeating-Characters-map.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        typedef map<char, size_t> HashType;
+        HashType hash;
+        size_t length = 0, idx = 0;
+        for (size_t i = 0; i < s.length(); i++) {
+            HashType::iterator itr = hash.lower_bound(s[i]);
+            if (itr != hash.end() && !(hash.key_comp()(s[i], itr->first))) {
+                if (itr->second >= idx) {
+                    length = max(length, i - idx);
+                    idx = itr->second + 1;
+                }
+                itr->second = i;
+            } else {
+                hash.insert(itr, HashType::value_type(s[i], i));
+            }
+        }
+        return max(length, s.length() - idx);
+    }
+};
