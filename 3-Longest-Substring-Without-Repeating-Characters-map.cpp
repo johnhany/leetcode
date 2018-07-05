777dc31d7ebee64f07b387a2845bb709e96d787c
John Hany <johnhany@163.com>
2018-07-05 22:35:16 +0800

    io speedup

--- 3-Longest-Substring-Without-Repeating-Characters-map.cpp
+++ 3-Longest-Substring-Without-Repeating-Characters-map.cpp
@@ -1,3 +1,11 @@
+static auto x = [](){
+    // turn off sync
+    std::ios::sync_with_stdio(false);
+    // untie in/out streams
+    cin.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
