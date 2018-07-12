5b8cc8f095d3396c6424aacf2e2a8e0203074678
John Hany <johnhany@163.com>
2018-07-12 23:01:59 +0800

    Create 10-Regular-Expression-Matching.cpp

---
+++ 10-Regular-Expression-Matching.cpp
@@ -0,0 +1,22 @@
+static auto x = [](){
+    // turn off sync
+    std::ios::sync_with_stdio(false);
+    // untie in/out streams
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length(), n = p.length(); 
+        vector<vector<bool> > dp(m + 1, vector<bool> (n + 1, false));
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (p[j - 1] == '*')
+                    dp[i][j] = dp[i][j - 2] || (i > 0 && (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j]);
+                else dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+        return dp[m][n];
+    }
+};
